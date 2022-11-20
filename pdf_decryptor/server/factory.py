0411ced64db8a508727b52ff8ec7059b36d188bf
Richard Kovacs <richardkovacs.it@gmail.com>
2022-11-20 12:49:34 +0100

    style: add style, center form

--- pdf_decryptor/server/factory.py
+++ pdf_decryptor/server/factory.py
@@ -5,7 +5,9 @@ from pdf_decryptor.api.main.api import blueprint as main_bp
 
 
 def create_app():
-    app = Flask(__name__, template_folder="base_templates", static_folder=None)
+    app = Flask(
+        __name__, template_folder="base_templates", static_folder="static"
+    )
     app.config.from_object("pdf_decryptor.server.config")
 
     app.register_blueprint(main_bp, url_prefix="/")
